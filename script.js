4041bab248ddd6788c38e248aa54ee2013283db5
lkarine <141235835+lkarine@users.noreply.github.com>
2025-03-17 10:39:09 -0300

    f

---
+++ script.js
@@ -0,0 +1,10 @@
+document.addEventListener("DOMContentLoaded,function"(){
+    const aumentaFonteBotao = document.getElementById('aumenta-fonte');
+    const diminuiFonteBotao = document.getElementById('diminui-fonte');
+
+    let tamanhoAtualFonte = 1;
+
+    
+
+
+}
